914dd6c07fba8a367789bce7541979aed1c7bb1f
Nikita Obrekht <obrekht@gmail.com>
2026-06-04 03:36:09 +0200

    fix(ui): adapt system bar icons to light/dark theme

--- app/src/main/java/me/obrekht/wishu/MainActivity.kt
+++ app/src/main/java/me/obrekht/wishu/MainActivity.kt
@@ -1,10 +1,8 @@
 package me.obrekht.wishu
 
 import android.os.Bundle
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -16,9 +14,8 @@ import me.obrekht.wishu.ui.WishlistScreen
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge(
-            statusBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
-        )
+        // Default auto styles: system bar icons follow light/dark theme.
+        enableEdgeToEdge()
         setContent {
             WishuTheme {
                 val navController = rememberNavController()
